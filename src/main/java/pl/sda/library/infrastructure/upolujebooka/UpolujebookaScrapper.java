62ea626cf6a1f86a99dad8ea8849fd14588b6b8a
pawel-matyaszczyk <pawel.matyaszczyk@gmail.com>
2020-11-06 00:36:28 +0100

    heroku api error

--- src/main/java/pl/sda/library/infrastructure/upolujebooka/UpolujebookaScrapper.java
+++ src/main/java/pl/sda/library/infrastructure/upolujebooka/UpolujebookaScrapper.java
@@ -34,7 +34,10 @@ class UpolujebookaScrapper {
     private static List<String> connectElements(Elements title, Elements authors, Elements isbn, Elements image, Elements description, Elements itemType) {
 
         List<String> foundElements = new ArrayList<>();
-        Integer minValue = Collections.min(List.of(title.size(),authors.size(), isbn.size(), image.size(), description.size(), itemType.size() ));
+        //Integer minValue = Collections.min(List.of(title.size(),authors.size(), isbn.size(), image.size(), description.size(), itemType.size()));
+
+        //heroku error with List.of!!!
+        Integer minValue =title.size();
 
         //String authorsList = authors.stream().map(Element::text).collect(Collectors.joining(","));
         String typeOfItem;
