f0ab42e1b0c3eaa949e33bc1122e4129a07df519
Tomasz Krutowski <tkrutowski@gmail.com>
2020-11-06 21:30:34 +0100

    update book

--- src/main/java/pl/sda/library/LibraryApplication.java
+++ src/main/java/pl/sda/library/LibraryApplication.java
@@ -2,8 +2,13 @@ package pl.sda.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
+
+        
 @SpringBootApplication
+//@EntityScan(basePackages = {"pl.sda.library.model"})
 public class LibraryApplication {
 
     public static void main(String[] args) {
