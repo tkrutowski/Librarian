06fd80260fe38361b4baac7f7af4b1c58f7308e1
pawel-matyaszczyk <pawel.matyaszczyk@gmail.com>
2020-11-15 13:58:51 +0100

    refactor CategoryDto, CategoryDtoRepository, DtoFactory

--- src/main/java/pl/sda/library/infrastructure/jpa/CategoryDto.java
+++ src/main/java/pl/sda/library/infrastructure/jpa/CategoryDto.java
@@ -1,14 +1,22 @@
 package pl.sda.library.infrastructure.jpa;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import pl.sda.library.domain.model.Category;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Setter
-@Getter
+@Data
 @Entity
 @Table(name = "categories")
 class CategoryDto {
@@ -18,10 +26,17 @@ class CategoryDto {
     private Long idCategory;
     private String name;
 
-    public Category toModel() {
-        Category category=new Category();
+    public Category toDomain() {
+        Category category = new Category();
         category.setIdCategory(getIdCategory());
         category.setName(getName());
         return category;
+    }
+
+    public CategoryDto fromDomain(Category category) {
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setIdCategory(category.getIdCategory());
+        categoryDto.setName(category.getName());
+        return categoryDto;
     }
 }
--- src/main/java/pl/sda/library/infrastructure/jpa/CategoryDtoRepository.java
+++ src/main/java/pl/sda/library/infrastructure/jpa/CategoryDtoRepository.java
@@ -1,8 +1,6 @@
 package pl.sda.library.infrastructure.jpa;
 
 import org.springframework.data.repository.CrudRepository;
-import pl.sda.library.infrastructure.jpa.CategoryDto;
-
 import java.util.Optional;
 
 interface CategoryDtoRepository extends CrudRepository<CategoryDto, Long> {
