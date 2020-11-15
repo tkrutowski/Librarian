a81b4a8553de0363ab02cecee4f47815bd1273d3
Tomasz Krutowski <tkrutowski@gmail.com>
2020-11-15 15:09:10 +0100

    refactor SeriesController

--- src/main/java/pl/sda/library/api/SeriesController.java
+++ src/main/java/pl/sda/library/api/SeriesController.java
@@ -9,32 +9,32 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/series")
+@RequestMapping("/api/series")
 public class SeriesController {
     SeriesService seriesService;
 
-    @GetMapping("/all")
-    public List<Series> getAllSeries(){
-        return seriesService.getAllSerieses();
-    }
-
-    @PostMapping("/add")
-    public Long addSeries(@RequestBody Series series)  {
+    @PostMapping
+    public Long addSeries(@RequestBody Series series) {
         return seriesService.addSeries(series);
     }
 
-    @PutMapping("/edit")
-    public Series editSeries(@RequestBody Series series)  {
-        return seriesService.editSeries(series);
-    }
-
-    @GetMapping("/{id}")
-    public Series getSeries(@PathVariable Long id){
-        return seriesService.getSeries(id);
+    @PutMapping("/{id}")
+    public Series editSeries(@RequestBody Series series, @PathVariable Long id) {
+        return seriesService.editSeries(series, id);
     }
 
     @DeleteMapping("/{id}")
-    public void delSeries(@PathVariable Long id){
+    public void deleteSeries(@PathVariable Long id) {
         seriesService.delSeries(id);
+    }
+
+    @GetMapping("/{id}")
+    public Series getSeries(@PathVariable Long id) {
+        return seriesService.getSeries(id);
+    }
+
+    @GetMapping
+    public List<Series> getAllSeries() {
+        return seriesService.getAllSerieses();
     }
 }
