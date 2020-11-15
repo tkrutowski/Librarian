82627d1645b001c3148753e342d40e06524b2d7a
Tomasz Krutowski <tkrutowski@gmail.com>
2020-11-15 19:25:24 +0100

    refactor SeriesService with test

--- src/test/java/pl/sda/library/service/SeriesServiceTest.java
+++ src/test/java/pl/sda/library/service/SeriesServiceTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.sda.library.LibraryApplication;
 import pl.sda.library.domain.model.Series;
+import pl.sda.library.domain.model.exception.SeriesAlreadyExistException;
+import pl.sda.library.domain.model.exception.SeriesDoesNotExistException;
 import pl.sda.library.domain.service.SeriesService;
-import pl.sda.library.domain.model.exception.ObjectAlreadyExistException;
-import pl.sda.library.domain.model.exception.ObjectDoesNotExistException;
 
 import static org.junit.Assert.*;
 
@@ -22,74 +22,76 @@ public class SeriesServiceTest {
     @Test
     public void should_return_id_bigger_than_zero_when_series_added() {
         //when
-        Series series=new Series(null,"Rambo","Doo jakiś opis");
+        Series series = new Series(null, "Rambo", "Doo jakiś opis");
 
         //given
         Long id = seriesService.addSeries(series);
 
         //then
-        assertNotEquals(java.util.Optional.of(0L),id);
+        assertNotEquals(java.util.Optional.of(0L), id);
     }
 
     @Test
-    public void should_throw_ObjectAlreadyExistException_when_series_already_exist()   {
+    public void should_throw_SeriesAlreadyExistException_when_series_already_exist() {
         //when
-        Series series=new Series(null,"Commando","Doo jakiś opis");
+        Series series = new Series(null, "Commando", "Doo jakiś opis");
         seriesService.addSeries(series);
 
         //then
-        assertThrows(ObjectAlreadyExistException.class, () -> seriesService.addSeries(series));
+        assertThrows(SeriesAlreadyExistException.class, () -> seriesService.addSeries(series));
     }
 
     @Test
-    public void should_return_changed_title_while_edit()  {
+    public void should_return_changed_title_while_edit() {
         //when
-        Series series=new Series(null,"Jack","Doo jakiś opis");
-        Long id =   seriesService.addSeries(series);
-        Series toEdit = seriesService.getSeries(id);
+        Series series = new Series(null, "Jack", "Doo jakiś opis");
+        Long id = seriesService.addSeries(series);
+        Series toEdit = seriesService.findSeries(id);
         toEdit.setTitle("Jack Ryan");
 
         //given
-        Series afterEdit = seriesService.editSeries(toEdit);
+        Series afterEdit = seriesService.editSeries(toEdit, id);
 
         //then
         assertEquals("Jack Ryan", afterEdit.getTitle());
     }
 
     @Test
-    public void should_throw_ObjectDoesNotExistException()   {
+    public void should_throw_SeriesDoesNotExistException_when_series_doesnt_exist() {
         //when
-        Series series=new Series(null,"Piraci","Doo jakiś opis");
-        Long id =   seriesService.addSeries(series);
-        Series toEdit = seriesService.getSeries(id);
+        Series series = new Series(null, "Piraci", "Doo jakiś opis");
+        Long id = seriesService.addSeries(series);
+        Long notExistID = 0L;
+        Series toEdit = seriesService.findSeries(id);
         toEdit.setTitle("Piraci z karaibów");
         toEdit.setId(0L);
 
         //then
-        assertThrows(ObjectDoesNotExistException.class, () -> seriesService.editSeries(toEdit));
+        assertThrows(SeriesDoesNotExistException.class, () -> seriesService.editSeries(toEdit, notExistID));
     }
 
     @Test
     public void should_return_size__plus_2_when_2_serieses_added() {
         //when
-        final int SIZE = seriesService.getAllSerieses().size() + 2;
-        seriesService.addSeries(new Series(null,"Niezniszczalni","Doo"));
-        seriesService.addSeries(new Series(null,"Powrót do przyszłości","Browm"));
+        final int SIZE = seriesService.findAllSerieses().size() + 2;
+        seriesService.addSeries(new Series(null, "Niezniszczalni", "Doo"));
+        seriesService.addSeries(new Series(null, "Powrót do przyszłości", "Browm"));
 
         //given
-        int result = seriesService.getAllSerieses().size();
+        int result = seriesService.findAllSerieses().size();
 
         //then
         assertEquals(SIZE, result);
     }
+
     @Test
     public void should_return_size__minus_1_when_one_series_deleted() {
         //when
-        final int SIZE = seriesService.getAllSerieses().size() -1;
-        seriesService.delSeries(2L);
+        final int SIZE = seriesService.findAllSerieses().size() - 1;
+        seriesService.deleteSeries(2L);
 
         //given
-        int result = seriesService.getAllSerieses().size();
+        int result = seriesService.findAllSerieses().size();
 
         //then
         assertEquals(SIZE, result);
