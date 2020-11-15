66189f76ebb29b9e37412f363ee4f90c6384fc60
pawel-matyaszczyk <pawel.matyaszczyk@gmail.com>
2020-11-15 13:45:58 +0100

    refactor Category.java

--- src/main/java/pl/sda/library/domain/model/Category.java
+++ src/main/java/pl/sda/library/domain/model/Category.java
@@ -1,13 +1,13 @@
 package pl.sda.library.domain.model;
 
-import lombok.*;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Setter
-@Getter
+@Data
 public class Category {
     private Long idCategory;
     private String name;
