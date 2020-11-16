71c2b89ffd6298f82b2d4649469833a95c32a56c
Tomasz Krutowski <tkrutowski@gmail.com>
2020-11-16 18:30:33 +0100

    refactor all repository classes

--- src/main/java/pl/sda/library/domain/port/BookRepository.java
+++ src/main/java/pl/sda/library/domain/port/BookRepository.java
@@ -2,21 +2,23 @@ package pl.sda.library.domain.port;
 
 import org.springframework.stereotype.Component;
 import pl.sda.library.domain.model.Book;
-import pl.sda.library.domain.model.User;
-
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public interface BookRepository {
 
-    boolean add(Book book);
+    Optional<Book> add(Book book);
 
     Optional<Book> edit(Book book);
 
     void delete(Long id);
 
-    List<Book> findAllByUser(User user);
+    List<Book> findAll();
 
     Optional<Book> findById(Long id);
+
+    List<Book> findAllByTitle(String title);
+
+    Optional<Book> findByTitle(String title);
 }
