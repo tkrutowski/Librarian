07367a5ec81cbffbf8cb39b2fc7fe828de15e054
Tomasz Krutowski <tkrutowski@gmail.com>
2020-11-16 18:31:47 +0100

    add/refactor exceptions

---
+++ src/main/java/pl/sda/library/domain/model/exception/BookAlreadyExistException.java
@@ -0,0 +1,9 @@
+package pl.sda.library.domain.model.exception;
+
+import pl.sda.library.domain.model.Book;
+
+public class BookAlreadyExistException extends ObjectAlreadyExistException {
+    public BookAlreadyExistException(Book book) {
+        super("Bookstore already exists: " + book.toString());
+    }
+}
