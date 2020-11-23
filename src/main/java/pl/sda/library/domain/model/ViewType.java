8908c4fabd6386ff9e9bc43d3c7a7f506c63de6e
Tomasz Krutowski <tkrutowski@gmail.com>
2020-11-23 22:33:46 +0100

    add BookViewOption, ViewType

---
+++ src/main/java/pl/sda/library/domain/model/ViewType.java
@@ -0,0 +1,8 @@
+package pl.sda.library.domain.model;
+
+//import org.springframework.stereotype.Component;
+//import java.lang.String;
+//@Component
+public enum ViewType {
+    SMALL, MEDIUM, LARGE
+}
