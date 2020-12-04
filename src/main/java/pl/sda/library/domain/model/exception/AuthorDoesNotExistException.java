c9dacb276183c0ed554d2e2f45b28d1112d76054
Tomasz Krutowski <tkrutowski@gmail.com>
2020-12-04 23:12:15 +0100

    add exception

--- src/main/java/pl/sda/library/domain/model/exception/AuthorDoesNotExistException.java
+++ src/main/java/pl/sda/library/domain/model/exception/AuthorDoesNotExistException.java
@@ -4,4 +4,7 @@ public class AuthorDoesNotExistException extends ObjectDoesNotExistException {
     public AuthorDoesNotExistException(Long id) {
         super("Author with id = " + id + " does not exist");
     }
+    public AuthorDoesNotExistException(String name) {
+        super("Author with name: " + name + " does not exist");
+    }
 }
